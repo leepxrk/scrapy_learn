6309fc07efcf9e699d9cbaedb5c8bf8ee4b319d0
leepxrk <leepx01rk@hotmail.com>
2017-04-28 20:30:54 +0800

    aa
    aaa

--- scrapy_learn1.py
+++ scrapy_learn1.py
@@ -1,15 +1,22 @@
-import scrapy
+# 图片链接列表，标题 
+# url是详情页链接 
+def getPiclink(url): 
+    sel = html.fromstring(requests.get(url).content) 
+    # 图片总数 倒数第二项里 
+    total = sel.xpath('//div[@class="pagenavi"]/a[last()-1]/span/text()')[0] 
+    # 标题 
+    title = sel.xpath('//h2[@class="main-title"]/text()')[0] 
 
+    # 接下来的链接放到这个列表 
+    jpgList = [] 
+    
+    for i in range(int(total)): 
+        # 每一页 
+        link = '{}/{}'.format(url, i+1) 
+        s = html.fromstring(requests.get(link).content) 
+        # 图片地址在src标签中 
+        jpg = s.xpath('//div[@class="main-image"]/p/a/img/@src')[0] 
+        # 图片链接放进列表 
+        jpgList.append(jpg) 
 
-class NgaSpider(scrapy.Spider):
-    name = "NgaSpider"
-    host = "http://bbs.ngacn.cc/"
-    # start_urls是我们准备爬的初始页
-    start_urls = [
-        "http://bbs.ngacn.cc/thread.php?fid=406",
-    ]
-
-    # 这个是解析函数，如果不特别指明的话，scrapy抓回来的页面会由这个函数进行解析。
-    # 对页面的处理和分析工作都在此进行，这个示例里我们只是简单地把页面内容打印出来。
-    def parse(self, response):
-        print response.body
+    return title, jpgList
